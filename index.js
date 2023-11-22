3ecd02fc5dc74bb6dd42006cf9dd45aa6f040c51
balijawa hussein <balijawahussein@gmail.com>
2023-11-22 23:12:59 +0300

    done with the templates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get("/",function(req, res){
-    res.render("app", {newitem :"ejs is working"});
+    res.render("app", {newitem :"ejs is working", today :"wednesday",time: 11.12});
 });
 
 app.listen(3000,()=>{console.log('its working sir')});
