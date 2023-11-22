81bf5ed1db8360ca4336045e3d2fea913a0bb480
balijawa hussein <balijawahussein@gmail.com>
2023-11-23 00:15:17 +0300

    made it work

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ const timeAndDay = new Date();
 // time auto refresh
 
 let time = "";
-
 const currentTime = setInterval(() => {
   const timeAndDay = new Date();
   time = timeAndDay.toLocaleTimeString();
 //   console.log(time);
 }, 1000);
-
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 const today = timeAndDay.toLocaleDateString("en-US", options);
+const items = [];
 app.get("/",function(req, res){
-    res.render("app", {newitem :"ejs is working", today :today,time:time});
+    res.render("app", {newListItems :items, today :today,time:time});
 });
-
+app.post("/",(req, res)=>{
+    const item = req.body.newItem;
+    items.push(item);
+    console.log(req.body);
+    res.redirect("/");
+});
 app.listen(3000,()=>{console.log('its working sir')});
