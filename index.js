ebf979241bc2f7548de2c94178883671b1a38193
balijawa hussein <balijawahussein@gmail.com>
2023-11-22 23:20:25 +0300

    time and day up

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const app = express();
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
+// getting the date and time
 
+const timeAndDay = new Date();
+const time = timeAndDay.toLocaleTimeString();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+const today = timeAndDay.toLocaleDateString();
 app.get("/",function(req, res){
-    res.render("app", {newitem :"ejs is working", today :"wednesday",time: 11.12});
+    res.render("app", {newitem :"ejs is working", today :today,time:time});
 });
 
 app.listen(3000,()=>{console.log('its working sir')});
